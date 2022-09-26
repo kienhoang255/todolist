74f5f117253a489f2541fe4eae48b27ee309d3d5
kienhoang255 <hoangtrungkien255@gmail.com>
2022-09-26 17:41:19 +0700

    test

---
+++ src/Pages/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./Header.module.scss";
+
+const cx = classNames.bind(styles);
+
+export default function Header() {
+  return <div className={cx("container")}>Test</div>;
+}
